861d436ddb69d910c1d40a918ca1670983083fa7
HearyShen <jiayun.shen@foxmail.com>
2020-03-03 23:30:00 +0800

    exclude reached words in BFS, success!

--- problems/word-transformer-lcci/solution.py
+++ problems/word-transformer-lcci/solution.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 from typing import List
 
 parser = argparse.ArgumentParser(__name__)
@@ -12,27 +13,34 @@ class Solution:
             return []
 
         bfs_count = 0
-        self.nextWords = dict()
         bfs_queue = []
         bfs_queue.append([beginWord])
+        self.rearchedWords.add(beginWord)
         while len(bfs_queue) > 0:
             cur_path = bfs_queue[0]
             cur_word = cur_path[-1]
             if bfs_count % args.print_freq == 0:
                 print(f"BFS count: {bfs_count}, Now: {cur_path}.")
             if cur_word == endWord:
+                print(f"BFS search count: {bfs_count}.")
                 return cur_path
 
-            nextStepWords = self.findNextWords(cur_word, endWord, words) - set(cur_path)
+            nextStepWords = self.findNextWords(cur_word, endWord, words) - self.rearchedWords
             for nextWord in nextStepWords:
                 nextPath = cur_path.copy()
                 nextPath.append(nextWord)
                 bfs_queue.append(nextPath)
 
+            self.rearchedWords = self.rearchedWords.union(nextStepWords)
+
             # basic BFS
             bfs_queue.remove(cur_path)
             bfs_count += 1
         return []
+
+    def __init__(self):
+        self.nextWords = dict()     # {key: wordGenericForm, value: words}
+        self.rearchedWords = set()  # rearched words shouldn't be searched again
 
     def findNextWords(self, beginWord: str, endWord: str, words: set) -> set:
         """Returns the words valid for next step."""
@@ -48,7 +56,6 @@ class Solution:
         else:
             return cachedNextWords
         
-
     @staticmethod
     def charDiffCount(srcWord: str, destWord: str) -> int:
         """Returns True if srcWord and destWord have only one char in difference."""
@@ -57,14 +64,18 @@ class Solution:
         return sum([int(not srcWord[i] == destWord[i]) for i in range(len(srcWord))])
 
 if __name__ == "__main__":
+    # # normal test
     # beginWord = "hit"
     # endWord = "cog"
     # wordList = ["hot","dot","dog","lot","log","cog"]
 
+    # large wordList test
     beginWord = "cet"
     endWord = "ism"
     wordList = ["kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"]
 
+    tic = time.time()
     ret = Solution().findLadders(beginWord, endWord, wordList)
+    toc = time.time()
     
-    print(ret)
+    print(f"Found: {ret} in {toc-tic:.3f}s.")
