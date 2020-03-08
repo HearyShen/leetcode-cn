46b0894b34d27fd21ba6aafc9da0f5812e45c3b9
HearyShen <jiayun.shen@foxmail.com>
2020-03-08 15:21:55 +0800

    speed up by skipping cycles

--- problems/prison-cells-after-n-days/solution.py
+++ problems/prison-cells-after-n-days/solution.py
@@ -1,31 +1,47 @@
 import time
 from typing import List
-from collections import defaultdict
 
 
 class Solution:
     def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
-        yesterday2today = defaultdict(list)
+        self.seenCells = {}
+        todayCells = cells
+        today = 0
+        cycle = None
+        while today < N:
+            cellsTuple = tuple(todayCells)  # convert to tuple (hashable)
+            try:
+                lastSeenDay = self.seenCells[cellsTuple]
+            except KeyError:
+                self.seenCells[cellsTuple] = today
+            else:
+                cycle = today - lastSeenDay
+                if N - today > cycle:  # if room to skip
+                    today += (
+                        (N - today) // cycle) * cycle  # then skip K-cycles
+                    continue  # IMPORTANT
 
-        cellsYesterday = [-1] + cells + [-1]
-        for day in range(N):
-            cellsToday = yesterday2today[tuple(cellsYesterday)]
-            if not cellsToday:
-                cellsToday = [-1] + [
-                    int(cellsYesterday[cellID - 1] == cellsYesterday[cellID + 1])
-                    for cellID in range(1,
-                                        len(cellsYesterday) - 1)
-                ] + [-1]
-                yesterday2today[tuple(cellsYesterday)] = cellsToday
-            cellsYesterday = cellsToday
-        return cellsToday[1:-1]
+            print(f"{today}/{N}: {todayCells}")
+            todayCells = self.findNextDay(todayCells)
+            today += 1
+
+        return todayCells
+
+    def findNextDay(self, cells: List[int]):
+        return [
+            int(cellID > 0 and cellID < len(cells) - 1
+                and cells[cellID - 1] == cells[cellID + 1])
+            for cellID in range(len(cells))
+        ]
+
 
 if __name__ == "__main__":
-    cells = [0, 1, 0, 1, 1, 0, 0, 1]
-    N = 7000000
+    testcases = [([1, 0, 0, 0, 1, 0, 0, 1], 99, [0, 0, 1, 0, 1, 0, 0, 0])]
 
-    tic = time.time()
-    ret = Solution().prisonAfterNDays(cells, N)
-    toc = time.time()
-
-    print(f"Caulated {N} days prison situation:\n{ret}\nin {toc-tic:.3f}s.")
+    for i, testcase in enumerate(testcases):
+        print(f"{i}: {testcase}")
+        cells, N, correctAns = testcase
+        tic = time.time()
+        ret = Solution().prisonAfterNDays(cells, N)
+        toc = time.time()
+        print(f"{i}: {ret==correctAns}\t({ret}) in {toc-tic:.3f}s.")
