05956c3da6b7fe5bf30c0bf581a6bb3bab65ef8b
HearyShen <jiayun.shen@foxmail.com>
2020-03-08 23:03:47 +0800

    implement BFS to search the shortest path, too slow for some testcases

--- problems/coin-change/solution.py
+++ problems/coin-change/solution.py
@@ -1,2 +1,43 @@
+import time
+from typing import List
+
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        """Use BFS to find the shortest coin change path."""
+        if amount == 0:
+            return 0
+
+        bfsQueue = []
+        for coin in coins:
+            bfsQueue.append([coin])
+
+        while bfsQueue:
+            curPath = bfsQueue[0]
+            
+            curSum = sum(curPath)
+            if curSum == amount:
+                # print(curPath)
+                return len(curPath)
+            elif curSum > amount:
+                pass    # do nothing, no bfsQueue add
+            else:
+                # find nexts
+                for coin in coins:
+                    if curSum + coin <= amount:
+                        bfsQueue.append(curPath + [coin])
+
+            bfsQueue.remove(bfsQueue[0])
+        return -1
+
+
+if __name__ == "__main__":
+    testcases = [([1, 2, 5], 11, 3), ([1], 0, 0), ([1,2,5], 100, 20)]
+
+    for i, testcase in enumerate(testcases):
+        coins, amount, ans = testcase
+        print(f"{i}: {testcase}")
+        tic = time.time()
+        ret = Solution().coinChange(coins, amount)
+        toc = time.time()
+        print(f"{i}: {ret == ans} ({ret}) in {toc-tic:.3f}s.")
