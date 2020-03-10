ab81be62d78b3f9d851c34eb674bbe17d9d10186
HearyShen <jiayun.shen@foxmail.com>
2020-03-10 10:32:56 +0800

    start solving

--- problems/minimum-remove-to-make-valid-parentheses/solution.py
+++ problems/minimum-remove-to-make-valid-parentheses/solution.py
@@ -1,2 +1,17 @@
+import time
+
+
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        # TODO: 2020.3.9
+
+
+if __name__ == "__main__":
+    testCases = [("lee(t(c)o)de)", "lee(t(c)o)de"), ("a)b(c)d", "ab(c)d"), ("))((", ""), ("(a(b(c)d)", "a(b(c)d)")]
+
+    for i, testCase in enumerate(testCases):
+        s, ans = testCase
+        tic = time.time()
+        ret = Solution().minRemoveToMakeValid(s)
+        toc = time.time()
+        print(f"{i}: {ret == ans}, {ret} in {toc-tic:.3f}s.")
