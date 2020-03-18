62fc3d76f14a879152556efc6c2a674589c13269
HearyShen <jiayun.shen@foxmail.com>
2020-03-18 23:46:46 +0800

    failed for "a","a" testcase due to infinite loop

--- problems/minimum-window-substring/solution.py
+++ problems/minimum-window-substring/solution.py
@@ -1,3 +1,39 @@
+import time
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # TODO: 2020.3.14
+        if not s or not t:
+            return ""
+        
+        l, r = 0, 0
+        minWindowLen = len(s)
+        targetChars = set(list(t))
+        while r <= len(s):
+            curChars = set(list(s[l:r]))
+            print(curChars)
+            if targetChars - curChars:
+                # if still need to match
+                r += 1
+            else:
+                # if all found
+                # shrink the left border
+                for newL in range(l, r):
+                    if targetChars - set(list(s[newL:r])):
+                        l = newL - 1
+                        if r-l <= minWindowLen:
+                            minWindowLen = r-l
+                            minWindowStr = s[l:r]
+                        l = newL
+                        break
+        return minWindowStr
+
+        
+
+if __name__ == "__main__":
+    testCases = [("ADOBECODEBANC", "ABC", "BANC"), ("a", "a", "a")]
+    for i, testCase in enumerate(testCases):
+        S, T, ans = testCase
+        tic = time.time()
+        ret = Solution().minWindow(S, T)
+        toc = time.time()
+        print(f"{i}: {ret == ans}, return {ret} in {toc-tic:.3f}s.")
