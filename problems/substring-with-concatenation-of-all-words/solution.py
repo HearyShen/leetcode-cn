859277e9f424ab94337757e2d1fd71f1edfa8d7d
HearyShen <jiayun.shen@foxmail.com>
2020-03-18 17:18:57 +0800

    use collections.Counter to count words

--- problems/substring-with-concatenation-of-all-words/solution.py
+++ problems/substring-with-concatenation-of-all-words/solution.py
@@ -1,32 +1,36 @@
 import time
 from typing import List
+from collections import Counter
+
 
 class Solution:
-    def isValidStr(self, ascWords: List[str], s: str) -> bool:
-        wordLen, wordCount = len(ascWords[0]), len(ascWords)
-        sWords = sorted([s[wordLen*i:wordLen*i+wordLen] for i in range(int(len(s)/wordLen))])
-        return ascWords == sWords
+    def isValidStr(self, wordsCounter: Counter, s: str, wordLen: int) -> bool:
+        wordsCounterOfS = Counter(
+            [s[i:i + wordLen] for i in range(0, len(s), wordLen)])
+        return wordsCounter == wordsCounterOfS
 
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if not s or not words:
             return []
 
         firstWords = set(words)
-        ascWords = sorted(words)
+        wordsCounter = Counter(words)
         wordLen, wordCount = len(words[0]), len(words)
         strLen = wordLen * wordCount
         validIndexes = []
         for i in range(len(s) - strLen + 1):
-            curWord = s[i:i+wordLen]
+            curWord = s[i:i + wordLen]
             if curWord in firstWords:
-                possibleStr = s[i:i+strLen]
-                if self.isValidStr(ascWords, possibleStr):
+                possibleStr = s[i:i + strLen]
+                if self.isValidStr(wordsCounter, possibleStr, wordLen):
                     validIndexes.append(i)
         return validIndexes
 
 
 if __name__ == "__main__":
-    testCases = [("barfoothefoobarman", ["foo","bar"], [0,9]), ("wordgoodgoodgoodbestword", ["word","good","best","word"], [])]
+    testCases = [("barfoothefoobarman", ["foo", "bar"], [0, 9]),
+                 ("wordgoodgoodgoodbestword", ["word", "good", "best",
+                                               "word"], [])]
 
     for i, testCase in enumerate(testCases):
         s, words, ans = testCase
