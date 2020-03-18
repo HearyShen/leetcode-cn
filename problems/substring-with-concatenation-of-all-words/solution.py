204641c380825101efb8c8ecf71af9b8f6ad4cf5
HearyShen <jiayun.shen@foxmail.com>
2020-03-18 17:10:13 +0800

    success with sliding window

--- problems/substring-with-concatenation-of-all-words/solution.py
+++ problems/substring-with-concatenation-of-all-words/solution.py
@@ -1,3 +1,36 @@
+import time
+from typing import List
+
 class Solution:
+    def isValidStr(self, ascWords: List[str], s: str) -> bool:
+        wordLen, wordCount = len(ascWords[0]), len(ascWords)
+        sWords = sorted([s[wordLen*i:wordLen*i+wordLen] for i in range(int(len(s)/wordLen))])
+        return ascWords == sWords
+
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        # TODO: 2020.3.13
+        if not s or not words:
+            return []
+
+        firstWords = set(words)
+        ascWords = sorted(words)
+        wordLen, wordCount = len(words[0]), len(words)
+        strLen = wordLen * wordCount
+        validIndexes = []
+        for i in range(len(s) - strLen + 1):
+            curWord = s[i:i+wordLen]
+            if curWord in firstWords:
+                possibleStr = s[i:i+strLen]
+                if self.isValidStr(ascWords, possibleStr):
+                    validIndexes.append(i)
+        return validIndexes
+
+
+if __name__ == "__main__":
+    testCases = [("barfoothefoobarman", ["foo","bar"], [0,9]), ("wordgoodgoodgoodbestword", ["word","good","best","word"], [])]
+
+    for i, testCase in enumerate(testCases):
+        s, words, ans = testCase
+        tic = time.time()
+        ret = Solution().findSubstring(s, words)
+        toc = time.time()
+        print(f"{i}: {set(ret) == set(ans)}， return {ret} in {toc-tic:.3f}s.")
