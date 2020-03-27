2cbac0f564f8b1587b14a444c4f754ed080ee1b0
HearyShen <jiayun.shen@foxmail.com>
2020-03-27 23:19:20 +0800

    passed with greedy algorithm, recursively find smallest letter from left

--- problems/remove-duplicate-letters/solution.py
+++ problems/remove-duplicate-letters/solution.py
@@ -1,3 +1,31 @@
+import time
+from collections import Counter
+
+
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        # TODO: 2020.3.26
+        """Greedy algorithm, recursively find smallest letter from left"""
+        if not s:
+            return ''
+
+        letters = Counter(s)
+        leftSmallestLetterIndex = 0
+        for i, letter in enumerate(s):
+            letters[letter] -= 1
+            if letter < s[leftSmallestLetterIndex]:
+                leftSmallestLetterIndex = i
+            if letters[letter] == 0:
+                break
+        curLetter = s[leftSmallestLetterIndex]
+        # remove all duplicate curLetter and recursive for greedy search in right string
+        return curLetter + self.removeDuplicateLetters(s[leftSmallestLetterIndex:].replace(curLetter, ''))
+
+
+if __name__ == "__main__":
+    testCases = [("bcabc", "abc"), ("cbacdcbc", "acdb"), ("baab", "ab"), ("bddbccd", "bcd")]
+    for i, testCase in enumerate(testCases):
+        s, ans = testCase
+        tic = time.time()
+        ret = Solution().removeDuplicateLetters(s)
+        toc = time.time()
+        print(f"{i}: {ret == ans}, return {ret} in {toc-tic:.3f}s.")
