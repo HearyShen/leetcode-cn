f250cd1ecbc82596124df536f4c4eb0bac610194
HearyShen <jiayun.shen@foxmail.com>
2020-04-01 19:41:29 +0800

    solve problem number-of-islands

---
+++ problems/number-of-islands/problem.md
@@ -0,0 +1,69 @@
+#### [200. 岛屿数量](https://leetcode-cn.com/problems/number-of-islands/)
+
+难度中等443收藏分享切换为英文关注反馈
+
+给定一个由 `'1'`（陆地）和 `'0'`（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+
+**示例 1:**
+
+```
+输入:
+11110
+11010
+11000
+00000
+
+输出: 1
+```
+
+**示例 2:**
+
+```
+输入:
+11000
+11000
+00100
+00011
+
+输出: 3
+```
+
+通过次数72,380
+
+提交次数151,257
+
+---
+
+#### [200. Number of Islands](https://leetcode-cn.com/problems/number-of-islands/)
+
+难度中等443收藏分享切换为中文关注反馈
+
+Given a 2d grid map of `'1'`s (land) and `'0'`s (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+**Example 1:**
+
+```
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+```
+
+**Example 2:**
+
+```
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
+```
+
+通过次数72,380
+
+提交次数151,257
---
+++ problems/number-of-islands/solution.py
@@ -0,0 +1,40 @@
+import time
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        self.grid = grid
+        self.islandsCount = 0
+        self.deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        
+        self.yLen, self.xLen = len(grid), len(grid[0])
+        for yIdx in range(self.yLen):
+            for xIdx in range(self.xLen):
+                if self.grid[yIdx][xIdx] == "1":
+                    hasIsland = self.bfs(yIdx, xIdx)
+                    if hasIsland:
+                        self.islandsCount += 1
+        return self.islandsCount
+                
+    def bfs(self, y, x):
+        if self.grid[y][x] == "0":
+            return False
+        
+        bfsQueue = [(y,x)]
+        self.grid[y][x] = "0"
+        while bfsQueue:
+            curY, curX = bfsQueue.pop(0)
+            
+            for delta in self.deltas:
+                nextY = curY + delta[0]
+                nextX = curX + delta[1]
+                if 0<=nextY<self.yLen and 0<=nextX<self.xLen and self.grid[nextY][nextX] == '1':
+                    bfsQueue.append((nextY, nextX))
+                    self.grid[nextY][nextX] = "0"   # NOTE: important, mark it once reach it to avoid repeating search
+        return True
+                
+        
