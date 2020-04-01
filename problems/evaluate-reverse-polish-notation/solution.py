4df89ec07f13f8d9e3020ddf3f90e786db42312d
HearyShen <jiayun.shen@foxmail.com>
2020-04-02 00:16:11 +0800

    passed with stack

--- problems/evaluate-reverse-polish-notation/solution.py
+++ problems/evaluate-reverse-polish-notation/solution.py
@@ -1,3 +1,39 @@
+import time
+from typing import List
+
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # TODO: 2020.4.1
+        stack = []
+        for token in tokens:
+            if not stack:
+                stack.append(token)
+            if token in {'+', '-', '*', '/'}:
+                y = stack.pop()
+                x = stack.pop()
+
+                if token == '+':
+                    stack.append(x+y)
+                elif token == '-':
+                    stack.append(x-y)
+                elif token == '*':
+                    stack.append(x*y)
+                elif token == '/':
+                    stack.append(int(x/y))
+            else:
+                stack.append(int(token))
+        return stack.pop()
+
+
+if __name__ == "__main__":
+    testCases = [
+        (["2", "1", "+", "3", "*"], 9), 
+        (["4", "13", "5", "/", "+"], 6),
+        (["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"], 22)
+    ]
+    for i, testCase in enumerate(testCases):
+        tokens, ans = testCase
+        tic = time.time()
+        ret = Solution().evalRPN(tokens)
+        toc = time.time()
+        print(f"{i}: {ret == ans}, return {ret} in {toc-tic:.3f}s.")
