6fcaab7df068fbb07093a26b089524f9333b84a6
HearyShen <jiayun.shen@foxmail.com>
2020-04-05 15:06:26 +0800

    passed with dfs

--- problems/clone-graph/solution.py
+++ problems/clone-graph/solution.py
@@ -6,12 +6,24 @@ class Node:
         self.neighbors = neighbors
 """
 
-class Node:
-    def __init__(self, val = 0, neighbors = []):
-        self.val = val
-        self.neighbors = neighbors
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        # TODO: 2020.4.4
-        pass
+        if not node:
+            return
+
+        visited = {}
+
+        def dfs(node):
+            if not node:
+                return
+            if node.val in visited:
+                return visited[node.val]
+            cloneNode = Node(node.val, [])
+            visited[cloneNode.val] = cloneNode
+            if node.neighbors:
+                for neighbor in node.neighbors:
+                    cloneNode.neighbors.append(dfs(neighbor))
+            return cloneNode
+
+        return dfs(node)
