236c1293439b74912ce16af62593b296fbf155c2
HearyShen <jiayun.shen@foxmail.com>
2020-04-05 14:36:58 +0800

    paste official solution here

--- problems/lfu-cache/solution.py
+++ problems/lfu-cache/solution.py
@@ -1,15 +1,101 @@
+import collections
+
+
+class Node:
+    def __init__(self, key, val, pre=None, nex=None, freq=0):
+        self.pre = pre
+        self.nex = nex
+        self.freq = freq
+        self.val = val
+        self.key = key
+
+    def insert(self, nex):
+        nex.pre = self
+        nex.nex = self.nex
+        self.nex.pre = nex
+        self.nex = nex
+
+
+def create_linked_list():
+    head = Node(0, 0)
+    tail = Node(0, 0)
+    head.nex = tail
+    tail.pre = head
+    return (head, tail)
+
+
 class LFUCache:
-    # TODO: 2020.4.5
     def __init__(self, capacity: int):
-        pass
+        self.capacity = capacity
+        self.size = 0
+        self.minFreq = 0
+        self.freqMap = collections.defaultdict(create_linked_list)
+        self.keyMap = {}
+
+    def delete(self, node):
+        if node.pre:
+            node.pre.nex = node.nex
+            node.nex.pre = node.pre
+            if node.pre is self.freqMap[
+                    node.freq][0] and node.nex is self.freqMap[node.freq][-1]:
+                self.freqMap.pop(node.freq)
+        return node.key
+
+    def increase(self, node):
+        node.freq += 1
+        self.delete(node)
+        self.freqMap[node.freq][-1].pre.insert(node)
+        if node.freq == 1:
+            self.minFreq = 1
+        elif self.minFreq == node.freq - 1:
+            head, tail = self.freqMap[node.freq - 1]
+            if head.nex is tail:
+                self.minFreq = node.freq
 
     def get(self, key: int) -> int:
-        pass
+        if key in self.keyMap:
+            self.increase(self.keyMap[key])
+            return self.keyMap[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        pass
+        if self.capacity != 0:
+            if key in self.keyMap:
+                node = self.keyMap[key]
+                node.val = value
+            else:
+                node = Node(key, value)
+                self.keyMap[key] = node
+                self.size += 1
+            if self.size > self.capacity:
+                self.size -= 1
+                deleted = self.delete(self.freqMap[self.minFreq][0].nex)
+                self.keyMap.pop(deleted)
+            self.increase(node)
+
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode-cn.com/problems/lfu-cache/solution/lfuhuan-cun-by-leetcode-solution/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+if __name__ == "__main__":
+    cache = LFUCache(2)  # capacity (缓存容量)
+
+    cache.put(1, 1)
+    cache.put(2, 2)
+    assert cache.get(1) == 1  # 返回 1
+    cache.put(3, 3)  # 去除 key 2
+    assert cache.get(2) == -1  # 返回 -1 (未找到key 2)
+    assert cache.get(3) == 3  # 返回 3
+    cache.put(4, 4)  # 去除 key 1
+    assert cache.get(1) == -1  # 返回 -1 (未找到 key 1)
+    assert cache.get(3) == 3  # 返回 3
+    assert cache.get(4) == 4  # 返回 4
+
+    print('passed')
