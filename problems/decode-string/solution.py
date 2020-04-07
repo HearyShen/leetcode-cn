c745d59e857d61d2bc1e06658fb8533e9c48c5b7
HearyShen <jiayun.shen@foxmail.com>
2020-04-07 23:19:34 +0800

    passed with auxiliary stack to save curMul and lastRes

--- problems/decode-string/solution.py
+++ problems/decode-string/solution.py
@@ -3,8 +3,30 @@ import time
 
 class Solution:
     def decodeString(self, s: str) -> str:
-        # TODO: 2020.4.7
-        pass
+        """
+        Use auxiliary stack to save curMul and lastRes.
+        Referenced from solution: https://leetcode-cn.com/problems/decode-string/solution/decode-string-fu-zhu-zhan-fa-di-gui-fa-by-jyd/
+        """
+        if not s:
+            return ''
+
+        stack = []
+        mul = 0
+        res = ''
+        for i, c in enumerate(s):
+            if c.isdigit():
+                mul = mul * 10 + int(c)
+            elif c == '[':
+                stack.append((mul, res))
+                res = ''
+                mul = 0
+            elif c.isalpha():
+                res += c
+            elif c == ']':
+                curMul, lastRes = stack.pop()
+                res = lastRes + curMul * res
+
+        return res
 
 
 if __name__ == '__main__':
