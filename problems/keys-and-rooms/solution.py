87d29f766078b4fcec79704eb568e89c8923de83
HearyShen <jiayun.shen@foxmail.com>
2020-04-09 00:31:23 +0800

    passed with BFS

--- problems/keys-and-rooms/solution.py
+++ problems/keys-and-rooms/solution.py
@@ -1,12 +1,27 @@
 import time
 from typing import List
+from collections import deque
 
 
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         # TODO: 2020.4.9
-        pass
+        """Solve with BFS"""
+        if not rooms:
+            return True
 
+        roomCount = len(rooms)
+        bfsQueue = deque()
+        bfsQueue.append(0)
+        unlocked = {0}
+        while bfsQueue and len(unlocked) < roomCount:
+            curRoom = bfsQueue.popleft()
+            nextRooms = [room for room in rooms[curRoom] if room not in unlocked]
+            bfsQueue.extend(nextRooms)
+            unlocked.update(nextRooms)
+        
+        return True if len(unlocked) == roomCount else False
+        
 
 if __name__ == "__main__":
     testCases = [([[1],[2],[3],[]], True), ([[1,3],[3,0,1],[2],[0]], False)]
