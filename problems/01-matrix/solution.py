7a42021a65561d002e3935990c69f5ce9e51d95e
HearyShen <jiayun.shen@foxmail.com>
2020-04-08 16:34:34 +0800

    passed with DP (2-pass DP, from left-top/right-down)

--- problems/01-matrix/solution.py
+++ problems/01-matrix/solution.py
@@ -7,16 +7,26 @@ class Solution:
         """Solve with DP"""
         # TODO: 2020.4.8
         rLen, cLen = len(matrix), len(matrix[0])
-        
+        # dist = [[10000] * cLen] * rLen    # ALERT: all the rows actually point to only one list
+        dist = [[1000000 for i in range(cLen)] for j in range(rLen)]
         for r in range(rLen):
             for c in range(cLen):
-                if matrix[r][c] == 1:
-                    up = matrix[r-1][c] if r-1 >=0 else 1
-                    down = matrix[r+1][c] if r+1 < rLen else 1
-                    left = matrix[r][c-1] if c-1 >= 0 else 1
-                    right = matrix[r][c+1] if c+1 < cLen else 1
-                    matrix[r][c] = min(up, down, left, right) + 1
-        return matrix
+                if matrix[r][c] == 0:
+                    dist[r][c] = 0
+                else:
+                    if r - 1>= 0:
+                        dist[r][c] = min(dist[r][c], dist[r-1][c] + 1)
+                    if c - 1 >= 0:
+                        dist[r][c] = min(dist[r][c], dist[r][c-1] + 1)
+
+        for r in range(rLen-1, -1, -1):     # dist_[rLen-1, -1) == dist_(rLen, 0]
+            for c in range(cLen-1, -1, -1):
+                if r + 1 < rLen:
+                    dist[r][c] = min(dist[r][c], dist[r+1][c] + 1)
+                if c + 1 < cLen:
+                    dist[r][c] = min(dist[r][c], dist[r][c+1] + 1)
+
+        return dist
 
 
 if __name__ == "__main__":
