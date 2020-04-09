b67552ab84a1a0ef68e177d8ede75dd35afbfd9a
HearyShen <jiayun.shen@foxmail.com>
2020-04-09 18:21:33 +0800

    passed with switching mode

--- problems/spiral-matrix/solution.py
+++ problems/spiral-matrix/solution.py
@@ -4,8 +4,29 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        # TODO: 2020.4.9
-        pass
+        if not matrix or not matrix[0]:
+            return []
+        outs = []
+        deltas = [[0,1], [1,0], [0,-1], [-1,0]]     # right, down, left, up
+        mode = 0
+        upBound, downBound, leftBound, rightBound = 0, len(matrix), 0, len(matrix[0])
+        r, c = 0, 0
+        while upBound <= r < downBound and leftBound <= c < rightBound:
+            outs.append(matrix[r][c])
+            if not (upBound <= r+deltas[mode][0] < downBound and leftBound <= c+deltas[mode][1] < rightBound):
+                if mode == 0:
+                    upBound += 1
+                elif mode == 1:
+                    rightBound -= 1
+                elif mode == 2:
+                    downBound -= 1
+                else:
+                    leftBound += 1
+                mode = (mode + 1) % 4   # change mode
+            # move to next according current mode
+            r += deltas[mode][0]
+            c += deltas[mode][1]
+        return outs
 
 
 if __name__ == "__main__":
